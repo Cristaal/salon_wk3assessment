8aa7d57f44a241e163dd416d978eb1f4f314d95b
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 14:36:24 -0800

    add spec

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,4 +1,5 @@
 require('spec_helper')
+require('pry')
 
 describe (Stylist) do
 
@@ -33,4 +34,15 @@ describe (Stylist) do
       expect(test_stylist1).to(eq(test_stylist2))
     end
   end
+
+  describe("#clients") do
+    it("it returns the clients array for the stylist") do
+      test_stylist = Stylist.new({:stylist_name => "Daniel", :id => nil})
+      test_stylist.save()
+      test_client = Client.new({:client_name => "Michael", :stylist_id => test_stylist.id, :id => nil})
+      test_client.save()
+      expect(test_stylist.clients()).to(eq([test_client]))
+    end
+  end
+
 end
