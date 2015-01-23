3bd306650797246f18500d1de15aea204a129a07
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 11:42:17 -0800

    add find spec

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,6 +8,16 @@ describe (Stylist) do
     end
   end
 
+  describe('.find') do
+    it("returns a stylist by its ID number") do
+      stylist1 = Stylist.new({:stylist_name => "Daniel", :id => nil})
+      stylist1.save()
+      stylist2 = Stylist.new({:stylist_name => "Kara", :id => nil})
+      stylist2.save()
+      expect(Stylist.find(stylist1.id())).to(eq(stylist1))
+    end
+  end
+
   describe("#save") do
     it("saves the stylist name to the salon database") do
       test_stylist = Stylist.new({:stylist_name => "Daniel", :id => nil})
@@ -19,10 +29,8 @@ describe (Stylist) do
   describe("#==") do
     it("is the same stylist if it has the same stylist name") do
       test_stylist1 = Stylist.new({:stylist_name => "Daniel", :id => nil})
-      test_stylist1.save()
-      test_stylist2 = Stylist.new({:stylist_name => "Kara", :id => nil})
-      test_stylist2.save()
-      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+      test_stylist2 = Stylist.new({:stylist_name => "Daniel", :id => nil})
+      expect(test_stylist1).to(eq(test_stylist2))
     end
   end
 end
