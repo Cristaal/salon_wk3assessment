9145e983f4a72d60bbee94f18d82e0bcce4cfbc3
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 14:35:21 -0800

    add method for pulling from data tables

--- lib/client.rb
+++ lib/client.rb
@@ -1,9 +1,10 @@
 class Client
-  attr_reader(:client_name, :id)
+  attr_reader(:client_name, :id, :stylist_id)
 
   define_method(:initialize) do |attributes|
     @client_name = attributes.fetch(:client_name)
     @id = attributes.fetch(:id)
+    @stylist_id = attributes.fetch(:stylist_id)
   end
 
   define_singleton_method(:all) do
@@ -12,20 +13,21 @@ class Client
     returned_clients.each() do |client|
       client_name = client.fetch("client_name")
       id = client.fetch("id").to_i()
-      clients.push(Client.new({:client_name => client_name, :id => id}))
+      stylist_id = client.fetch("stylist_id").to_i()
+      clients.push(Client.new({:client_name => client_name, :id => id, :stylist_id => stylist_id}))
     end
     clients
   end
 
   define_method(:save) do
-    result = DB.exec("INSERT INTO clients (client_name) VALUES ('#{@client_name}') RETURNING id;")
+    result = DB.exec("INSERT INTO clients (client_name, stylist_id) VALUES ('#{@client_name}', #{@stylist_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
 
   define_method(:==) do |another_client|
     self.client_name().==(another_client.client_name()).&(self.id().==(another_client.id()))
   end
-  
+
   define_singleton_method(:find) do |id|
     found_client = nil
     Client.all().each() do |client_name|
