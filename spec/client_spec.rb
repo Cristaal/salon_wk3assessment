f5481c7194876646b3b3f455f457f86d33e577f8
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 12:40:54 -0800

    add specs for clients class

---
+++ spec/client_spec.rb
@@ -0,0 +1,37 @@
+require('spec_helper')
+
+
+describe(Client) do
+
+  describe(".all") do
+    it("is empty at first") do
+     expect(Client.all()).to(eq([]))
+    end
+  end
+
+  describe(".find") do
+    it("returns a client by its ID number") do
+      client1 = Client.new({:client_name => "Michael", :id => nil})
+      client1.save()
+      client2 = Client.new({:client_name => "Crystal", :id => nil})
+      client2.save()
+      expect(Client.find(client1.id())).to(eq(client1))
+    end
+  end
+
+  describe("#save") do
+    it("pushes the client name into the clients array") do
+      test_client = Client.new({:client_name => "Crystal", :id => nil})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same client name") do
+      test_client1 = Client.new({:client_name => "Crystal", :id => nil})
+      test_client2 = Client.new({:client_name => "Crystal", :id => nil})
+      expect(test_client1).to(eq(test_client2))
+    end
+  end
+end
