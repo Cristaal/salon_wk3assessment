429fe47e08c35767f12f41cd48fa587afd033d0f
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 14:35:42 -0800

    add method

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,5 +1,6 @@
+require('pry')
 class Stylist
-  attr_reader(:stylist_name, :id, :stylist_id)
+  attr_reader(:stylist_name, :id)
 
   define_method(:initialize) do |attributes|
     @stylist_name = attributes.fetch(:stylist_name)
@@ -35,4 +36,17 @@ class Stylist
     end
     found_stylist
   end
+
+  define_method(:clients) do
+    list_clients = []
+    clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()};")
+    clients.each() do |client|
+      client_name = client.fetch("client_name")
+      stylist_id = client.fetch("stylist_id").to_i()
+      id = client.fetch("id").to_i()
+      list_clients.push(Client.new({:client_name => client_name, :id => id, :stylist_id => stylist_id}))
+    end
+    list_clients
+  end
+
 end
