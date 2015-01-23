19252bfb314200b5565f73181e0adb334cc86849
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 11:12:40 -0800

    add save and == specs

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -7,4 +7,22 @@ describe (Stylist) do
       expect(Stylist.all()).to(eq([]))
     end
   end
+
+  describe("#save") do
+    it("saves the stylist name to the salon database") do
+      test_stylist = Stylist.new({:stylist_name => "Daniel", :id => nil})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same stylist if it has the same stylist name") do
+      test_stylist1 = Stylist.new({:stylist_name => "Daniel", :id => nil})
+      test_stylist1.save()
+      test_stylist2 = Stylist.new({:stylist_name => "Kara", :id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+    end
+  end
 end
