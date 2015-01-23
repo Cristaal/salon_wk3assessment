6d00925f5c0331e9ee79a4f2f1e8e532b89fa899
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-01-23 12:41:25 -0800

    add methods for client class

---
+++ lib/client.rb
@@ -0,0 +1,38 @@
+class Client
+  attr_reader(:client_name, :id)
+
+  define_method(:initialize) do |attributes|
+    @client_name = attributes.fetch(:client_name)
+    @id = attributes.fetch(:id)
+  end
+
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients")
+    clients = []
+    returned_clients.each() do |client|
+      client_name = client.fetch("client_name")
+      id = client.fetch("id").to_i()
+      clients.push(Client.new({:client_name => client_name, :id => id}))
+    end
+    clients
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO clients (client_name) VALUES ('#{@client_name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_method(:==) do |another_client|
+    self.client_name().==(another_client.client_name()).&(self.id().==(another_client.id()))
+  end
+  
+  define_singleton_method(:find) do |id|
+    found_client = nil
+    Client.all().each() do |client_name|
+      if client_name.id().==(id)
+        found_client = client_name
+      end
+    end
+    found_client
+  end
+end
